93145895ce75437240e1d5c30839864e82fafd27
corbalanstefanis-blip <corbalanstefanis@gmail.com>
2026-09-20 13:39:12 -0300

    Corregir reprogramación, cancelación y avisos al cliente

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='mi-negocio-barberia-v19-barberia-central';
+const CACHE='mi-negocio-barberia-v20-turnos-sync';
 const ASSETS=['./','./index.html','./reservas.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 
 self.addEventListener('install',event=>{
